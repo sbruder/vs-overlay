6834478f330305d342dc1c18d44535bd4fbb499d
Simon Bruder <simon@sbruder.de>
2021-05-14 23:15:31 +0200

    vapoursynthPlugins.placebo: fix build

--- plugins/placebo/default.nix
+++ plugins/placebo/default.nix
@@ -29,6 +29,10 @@ stdenv.mkDerivation rec {
       url = "https://github.com/Lypheo/vs-placebo/commit/d717bb49ce03ea9d67152a7c2e4df026de040c35.diff";
       sha256 = "0phnhpv7alxyw4ki3kj3a19j8qv16hww6gz06lyiyabj20xqibri";
     })
+    (fetchpatch {
+      url = "https://github.com/Lypheo/vs-placebo/commit/6a99aa1c427f9119d695d3e9d1a08782f51740d1.diff";
+      sha256 = "1x7by89vc0368p4baqlyqi5kw38plr024g4kxbr6afg4f8q4rl58";
+    })
   ];
 
   nativeBuildInputs = [ meson ninja pkg-config ];
